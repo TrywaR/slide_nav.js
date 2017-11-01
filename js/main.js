3d6f17d09a6178b6c0c6d669d43352d9c0fe2f49
Sergey Ilin <trywar@yandex.ru>
2017-11-02 00:44:47 +0300

    event
    $('.class').slider()

--- js/main.js
+++ js/main.js
@@ -1,49 +1,62 @@
-$(function(){
-	var
-		slider = $('.slider'),
-		slide = slider.find('._slide'),
-		menu = $('#menu'),
-		menu_items = menu.find('li'),
-		index_x = 0,
-		index_y = 0,
-		menu_html = ''
-
-	slider.css({
-		'position': 'fixed',
-		'top': '0',
-		'left': '0',
-		'right': '0',
-		'bottom': '0',
-		'transition': 'all 1s ease 0s'
-	})
-
-	$.each(slide, function(){
+(function($){
+	$.fn.slider = function(options) {
 		var
-			x = $(this).data('x')*100+'vw',
-			y = $(this).data('y')*100+'vh'
+			options = $.extend({
+				slide: '._slide',
+				menu: '#menu',
+			},
+			options)
 
-		$(this).css({
-			'position': 'absolute',
-			'top': y,
-			'left': x,
-			'width': '100vw',
-			'height': '100vh'
+		return this.each(function() {
+			var
+				slider = $(this),
+				slide = slider.find(options.slide),
+				menu = $(options.menu),
+				menu_items = menu.find('li'),
+				index_x = 0,
+				index_y = 0,
+				menu_html = ''
+
+			slider.css({
+				'position': 'fixed',
+				'top': '0',
+				'left': '0',
+				'right': '0',
+				'bottom': '0',
+				'transition': 'all 1s ease 0s'
+			})
+
+			$.each(slide, function(){
+				var
+					x = $(this).data('x') ? $(this).data('x')*100+'vw' : '0vw',
+					y = $(this).data('y') ? $(this).data('y')*100+'vh' : '0vh',
+					w = $(this).data('w') ? $(this).data('w')*100+'vw' : '100vw',
+					h = $(this).data('h') ? $(this).data('h')*100+'vh' : '100vh'
+
+				$(this).css({
+					'position': 'absolute',
+					'top': y,
+					'left': x,
+					'width': w,
+					'height': h
+				})
+			})
+
+			menu_items.on('click', function(){
+				var
+				x = slider.find(':eq('+$(this).index()+')').data('x'),
+				y = +slider.find(':eq('+$(this).index()+')').data('y')
+
+				x = x!=0 ? '-'+x*100+'vw' : 0 ;
+				y = y!=0 ? '-'+y*100+'vh' : 0 ;
+
+				slider.css({
+					'top': y,
+					'left': x
+				})
+
+				slider.find(':eq('+$(this).index()+')').addClass('active').siblings().removeClass('active')
+			})
 		})
-	})
-
-	menu_items.on('click', function(){
-		var
-			x = slider.find(':eq('+$(this).index()+')').data('x'),
-			y = +slider.find(':eq('+$(this).index()+')').data('y')
-
-		x = x!=0 ? '-'+x*100+'vw' : 0 ;
-		y = y!=0 ? '-'+y*100+'vh' : 0 ;
-
-		slider.css({
-			'top': y,
-			'left': x
-		})
-
-		slider.find(':eq('+$(this).index()+')').addClass('active').siblings().removeClass('active')
-	})
-})
+	}
+})(jQuery);
